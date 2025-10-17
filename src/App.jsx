165ccc5a46e81547227657385e044f8147014ffe
Dudz <142178060+Odudz@users.noreply.github.com>
2025-10-17 08:35:34 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod corrupti cum deserunt consequatur odio temporibus laboriosam dignissimos fuga laborum quisquam qui, accusamus cupiditate doloribus autem repudiandae repellendus expedita libero consectetur.</h1>
+    </>
+  )
+}
+
+export default App
